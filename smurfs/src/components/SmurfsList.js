fe3be0d219a986bb4ded0ebc5e8a0704852884b9
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-02-10 23:52:25 -0500

    refactored to Redux with Hooks

--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -1,39 +1,38 @@
 import React, { useEffect } from "react";
-import { connect } from "react-redux";
-
-import { fetchEntry } from '../actions/index';
+import { useSelector, useDispatch } from "react-redux";
+import { fetchEntry } from "../actions/index";
 
 function SmurfsList(props) {
-
-    useEffect(() => {
-        props.fetchEntry();
-    }, [])
-
-  console.log(props)  
-  return (
-    <div className="List">
-      <div>
-          {props.smurfs.map((smurf, index) => {
-              return <div key={index}>
-                  <h1>{smurf.name}</h1>
-                  <h3>{smurf.age}</h3>
-                  <h3>{smurf.height}</h3>
-              </div>
-          })}
-      </div>
-    </div>
-  );
-}
-
-const mapStateToProps = state => {
+  const state = useSelector(state => {
     return {
       smurfs: state.smurfs,
       error: state.error,
       isFetching: state.isFetching
     };
-  };
-  
-  export default connect(
-    mapStateToProps,
-    { fetchEntry }
-  )(SmurfsList);
+  });
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchEntry());
+  }, []);
+
+  console.log(props);
+  return (
+    <div className="List">
+      <div>
+        {state.smurfs.map((smurf, index) => {
+          return (
+            <div key={index}>
+              <h1>{smurf.name}</h1>
+              <h3>{smurf.age}</h3>
+              <h3>{smurf.height}</h3>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default SmurfsList;
